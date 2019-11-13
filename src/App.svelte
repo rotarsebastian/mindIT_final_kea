af79ec31352c578836211e53aa4e30942bcd84ce
Sebastian Rotar <rotar.seby1@gmail.com>
2019-11-13 11:06:30 +0100

    connected to backend

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,21 @@
 <script>
+	import axios from "axios";
+	import jq from "jquery";
+
+
+	const getData = () => {
+		// axios.get('https://raw.githubusercontent.com/rotarsebastian/node-task/master/data.json').then(response => console.log(response.data));
+		axios.get('http://localhost:8080/backend/apis/api-get-riddles.php').then(response => console.log(response.data));
+	};
+
+	getData();
+
+	// 	getNewsfeed(query: any = {}): Observable<any> {
+	//     const { page = 1, limit = 10 } = query;
+	//     const url = `${this.baseUrl}/${this.organizationId}/newsfeed-v2`;
+	//     return this.http.get<any>(url, { params: { page, limit } });
+	//   }
+	
 	let name = 'world';
 </script>
 
