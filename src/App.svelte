f9746f00dfefeef8b503b9662d48389acafe70d0
Sebastian Rotar <rotar.seby1@gmail.com>
2019-11-15 12:30:30 +0100

    Added interface

--- src/App.svelte
+++ src/App.svelte
@@ -9,10 +9,11 @@
 	};
 
 	const addRiddle = () => {
+		console.log(text, lastName, email);
 		axios.post(basicURL + 'api-create-riddle.php', {
-			riddle_text: 'riddle3',
-			lastName: 'Flintstone',
-			email: 'c@c.com'
+			riddle_text: text,
+			lastName: lastName,
+			email: email
 		})
 		.then(function (response) {
 			console.log(response.data);
@@ -25,7 +26,50 @@
 	getData();
 	
 	let name = 'world';
+	let text = '';
+	let lastName = '';
+	let email = '';
+
 </script>
 
+<!-- ****************************************************** -->
+<!-- ************************Styles************************ -->
+<!-- ****************************************************** -->
+
+<style>
+	.inputElement{
+		display: flex;
+		justify-content: space-between;
+		width: 300px;
+		height: auto;
+	}
+</style>
+
+<!-- ****************************************************** -->
+<!-- ************************HTML************************** -->
+<!-- ****************************************************** -->
+
 <h1>Hello {name}!</h1>
+
+<div class="inputElement">
+	<label for="text">
+		Riddle Text
+	</label>
+	<input id="text" bind:value={text}>
+</div>
+
+<div class="inputElement">
+	<label for="lastName">
+		Last name
+	</label>
+	<input id="lastName" bind:value={lastName}>
+</div>
+
+<div class="inputElement">
+	<label for="email">
+		Email
+	</label>
+	<input id="email" bind:value={email}>
+</div>
+
 <button on:click={addRiddle}>Add riddle</button>
