9482d2c3a78c078abfcb4177c013ef1e25bf6652
Sebastian Rotar <rotar.seby1@gmail.com>
2019-11-25 16:24:28 +0100

    validation done

--- src/pages/CreateQuiz.svelte
+++ src/pages/CreateQuiz.svelte
@@ -4,16 +4,20 @@
 	const basicURL = 'https://aqueous-escarpment-49631.herokuapp.com/apis/';
 	let arrayOfQuestions = [];
 
-	let questionValue = '', questionAnswer = '', questionDifficulty, canCreateQuiz = false;
+	let questionValue = '', questionAnswer = '', questionDifficulty = 0, canCreateQuiz = false, difficultyChoosed = false;
 	const difficultyLevel = [
-		{ value: '', text: 'Select difficulty level' },
-		{ value: 'easy', text: 'Easy' },
-		{ value: 'medium', text: 'Medium' },
-		{ value: 'hard', text: 'Hard' }
+		{ value: 0, text: 'Select difficulty level' },
+		{ value: 1, text: 'Easy' },
+		{ value: 2, text: 'Medium' },
+		{ value: 3, text: 'Hard' }
 	];
 
 	const addQuestion = () => {
-		if (questionDifficulty !== '' && questionValue.length > 5 && questionAnswer.length > 5) {
+		if (questionDifficulty === 0) {
+			difficultyChoosed = true;
+		}
+		if (questionDifficulty !== 0 && questionValue.length > 5 && questionAnswer.length > 5) {
+			difficultyChoosed = false;
 			const newQuestion = {
 				questionContentValue: questionValue,
 				questionAnswerValue: questionAnswer,
@@ -40,11 +44,11 @@
 		});
 	};
 	
-	const isNotValid = (value) => {
-		if( value.length > 5 ) {
-			return false;
+	const showError = (value) => {
+		if( value.length <= 5 && value.length > 0 ) {
+			return true;
 		} 
-		return true;
+		return false;
 	}
 
 </script>
@@ -58,6 +62,8 @@
 
 	.error {
 		color: red;
+		font-size: 0.7rem;
+    	margin: .5rem 0;
 	}
 </style>
 
@@ -71,7 +77,7 @@
 			</label>
 			<input id="text" bind:value={questionValue}>
 		</div>
-		{#if isNotValid(questionValue) }
+		{#if showError(questionValue) }
 			<div class="error">Not long enough</div>
 		{/if}
 
@@ -79,9 +85,9 @@
 			<label for="lastName">
 				Answer
 			</label>
-			<input id="lastName" bind:value={questionAnswer} name="questionAnswer">
+				<input id="lastName" bind:value={questionAnswer} name="questionAnswer">
 		</div>
-		{#if isNotValid(questionAnswer) }
+		{#if showError(questionAnswer) }
 			<div class="error">Not long enough</div>
 		{/if}
 
@@ -94,6 +100,11 @@
 				{/each}
 			</select>
 		</div>
+
+		{#if difficultyChoosed && questionDifficulty === 0 }
+			<div class="error">Please select difficulty</div>
+		{/if}
+
 		<button on:click={ addQuestion } >Add question</button>
 </div>
 
